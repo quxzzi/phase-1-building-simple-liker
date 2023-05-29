fd25c214475cc0a02eaf8e1c9f328b1a263af1c0
Kevin Dean <kdean1011@gmail.com>
2023-05-29 03:50:40 -0400

    done

--- main.js
+++ main.js
@@ -2,9 +2,49 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const errorModal = document.getElementById("modal");
+  const likeButtons = document.querySelectorAll(".like-glyph");
 
+  errorModal.classList.add("hidden");
 
+  likeButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          button.classList.toggle("activated-heart");
+          button.classList.toggle("like");
+          if (button.classList.contains("activated-heart")) {
+            button.innerHTML = FULL_HEART;
+          } else {
+            button.innerHTML = EMPTY_HEART;
+          }
+        })
+        .catch(() => {
+          errorModal.classList.remove("hidden");
+          const errorMessage = document.getElementById("modal-message");
+          errorMessage.textContent = "Error: Server request failed.";
+
+          setTimeout(() => {
+            errorModal.classList.add("hidden");
+          }, 3000);
+        });
+    });
+  });
+});
+
+function mimicServerCall() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const random = Math.random();
+      if (random < 0.5) {
+        resolve("success");
+      } else {
+        reject("error");
+      }
+    }, 300);
+  });
+}
 
 
 //------------------------------------------------------------------------------
